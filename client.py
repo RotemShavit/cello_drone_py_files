0f9888b3bea727b652a9be0ae5bf664b7ce0efe9
RotemShavit <50513126+RotemShavit@users.noreply.github.com>
2020-04-29 19:10:01 +0300

    latest server and client - reconnecting working

--- client.py
+++ client.py
@@ -6,28 +6,40 @@ import time
 # SOCK_STREAM == TCP
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('147.234.54.189', 1234))
-print("connected")
+# s.connect(('147.234.54.189', 12341))
+s.connect((socket.gethostname(), 12341))
 
-counter = 0
-msg = ""
-fin_msg = bytes("finished", "utf-8")
-while msg != fin_msg:
-    msg = s.recv(1024)
-    s.sendall(bytes(str(counter), "utf-8"))
-    print(msg)
+print("connected\n")
+s.sendall(bytes("connected", "utf-8"))
+
+while True:
+    to_send = "CD;"
+    # req_data = [0, 0, 0]
+    # req_data_str = ["Battery", "Armed", "Status"]
+    # i = 0
+    # for d in req_data:
+    #     if d == 1:
+    #         to_send = to_send + req_data_str[i] + ";"
+    #     i += 1
+    # to_send_lst = to_send.split(";")
+    # print(to_send)
+    s.sendall(bytes(to_send, "utf-8"))
+    # s_msg = str(s.recv(1024))
+    # s_msg = s_msg[2:-2]
+    # data = str(s_msg).split(";")
+    # i = 1
+    # for d in data:
+    #     if "Battery" in d:
+    #         temp_d = d[8:21]
+    #         vol = float(temp_d[8:13])
+    #         print(vol)
+    #         print(to_send_lst[i] + ": " + temp_d)
+    #     else:
+    #         print(to_send_lst[i] + ": " + d)
+    #     i += 1
     time.sleep(1)
-    counter += 1
 
-# s.sendall(bytes("Client is sending\n:", "utf-8"))
-# counter = 0
-# while counter < 5:
-#     s.sendall(bytes(str(counter), "utf-8"))
-#     time.sleep(1)
-#     counter += 1
-#s.sendall(bytes("finished", "utf-8"))
 time.sleep(2)
-
 s.shutdown(socket.SHUT_RDWR)
 time.sleep(3)
 s.close()
--- server.py
+++ server.py
@@ -1,38 +1,64 @@
 import socket
 import time
+from dronekit import *
+from datetime import datetime
 
 # create the socket
 # AF_INET == ipv4
 # SOCK_STREAM == TCP
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 1234))
+s.bind(('', 12341))
 s.listen(5)
 
-client_socket, address = s.accept()
+# cello_drone = connect("/dev/ttyS0", wait_ready=True, baud=57600)
+
+# print("mav is up")
+
+s_client_socket, s_address = s.accept()
+
 # accept only our ip
-print("Connection from rpi (addr = ", address, ") has been established!")
-client_socket.sendall("Server is sending:")
+print("Connection from rpi (addr = ", s_address, ") has been established!")
 time.sleep(1)
-counter = 0
 
-while counter < 11:
-    client_socket.sendall(str(counter))
-    msg=client_socket.recv(1024)
-    print(msg)
-    time.sleep(1)
-    counter = counter + 1
-client_socket.sendall("finished")
+start_time = time.time()
+
+active = True
+
+empty_msg = bytes("", "utf-8")
+
+while True:
+    to_send = ""
+    if active:
+        s_msg = s_client_socket.recv(1024)
+        elapsed_time = time.time() - start_time
+        if s_msg != empty_msg:
+            start_time = time.time()
+            print(str(s_msg) + " " + str(datetime.now()))
+            print("time elapsed = ", elapsed_time)
+        if elapsed_time > 20:
+            print("Connection lost")
+            s.close()
+            active = False
+    else:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', 12341))
+        s.listen(5)
+        print("Trying to reconnect...")
+        s_client_socket, s_address = s.accept()
+        print("Reconnected")
+        start_time = time.time()
+        active = True
+
+    # if "Battery" in s_msg:
+    #     to_send = to_send + str(cello_drone.battery)+";"
+    # if "Armed" in s_msg:
+    #     to_send = to_send + str(cello_drone.armed) + ";"
+    s_client_socket.sendall(bytes(to_send, "utf-8"))
+    # time.sleep(1)
+
+s_client_socket.sendall("finished")
 time.sleep(2)
-
-#fin_msg = bytes("finished")
-#msg =""
-#while msg != fin_msg:
-#    msg=client_socket.recv(1024)
-#    print(msg)
-#    counter += 1
-#    time.sleep(1)
-
 s.shutdown(socket.SHUT_RDWR)
 time.sleep(3)
 s.close()
